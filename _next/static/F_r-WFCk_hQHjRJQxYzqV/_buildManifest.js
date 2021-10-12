c68652a9d433aabd1f4d54c4d96b891bf729bd98
shizoe <71692698+shizoe@users.noreply.github.com>
2021-10-12 12:41:30 +0000

    Deploying to gh-pages from @ shizoe/cv@fb309d4275e660f4250e6a7d13ec7817339fe15f 🚀

--- _next/static/F_r-WFCk_hQHjRJQxYzqV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/291-9e59219a99203b2ea1c9.js","static/chunks/pages/index-bd642a941632c7115dc5.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
